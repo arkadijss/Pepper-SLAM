b654106fbdb21a7f7184afdfb9506343fca62afc
“arkadijss” <80927090+arkadijss@users.noreply.github.com>
2024-04-17 20:43:41 +0000

    synchronized camera info for the estimated depth image

--- src/perception/src/depth_estimation.py
+++ src/perception/src/depth_estimation.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
 import rospy
-from sensor_msgs.msg import Image
+from sensor_msgs.msg import Image, CameraInfo
 from cv_bridge import CvBridge
 import torch
 import numpy as np
@@ -10,40 +10,60 @@ import scripts.depth_estimation.metric_depth.estimate as estimate
 
 
 class DepthEstimation:
-    def __init__(self, image_topic="~/camera/image_raw", depth_topic="~/depth_nn"):
+    def __init__(
+        self,
+        img_topic="~/camera/image_raw",
+        cam_info_topic="~/camera/camera_info",
+        depth_topic="~/depth_nn/image",
+        depth_nn_cam_info_topic="~/depth_nn/camera_info",
+    ):
         rospy.init_node("depth_estimation_node", anonymous=True)
-        self.model = estimate.build_model_metric()
         self.bridge = CvBridge()
-        self.image_subscriber = rospy.Subscriber(
-            image_topic, Image, self.image_callback
+        self.model = estimate.build_model_metric()
+
+        self.cam_info = None
+        self.cam_info_sub = rospy.Subscriber(
+            cam_info_topic, CameraInfo, self.cam_info_callback
         )
-        self.depth_publisher = rospy.Publisher(depth_topic, Image, queue_size=10)
+        self.img_sub = rospy.Subscriber(img_topic, Image, self.img_callback)
 
-    def image_callback(self, ros_image):
+        self.depth_pub = rospy.Publisher(depth_topic, Image, queue_size=10)
+        self.depth_nn_cam_info_pub = rospy.Publisher(
+            depth_nn_cam_info_topic, CameraInfo, queue_size=10
+        )
+
+    def cam_info_callback(self, cam_info):
+        self.cam_info = cam_info
+
+    def img_callback(self, ros_img):
         try:
-            cv_image = self.bridge.imgmsg_to_cv2(ros_image, "bgr8")
-            img_torch = estimate.prepare_sample(cv_image).cuda()
+            cam_info = self.cam_info
+            cv_img = self.bridge.imgmsg_to_cv2(ros_img, "bgr8")
+            img_torch = estimate.prepare_sample(cv_img).cuda()
 
-            predicted_depth = estimate.infer(self.model, img_torch)
+            pred_depth = estimate.infer(self.model, img_torch)
 
             # interpolate to original size
-            prediction = torch.nn.functional.interpolate(
-                predicted_depth,
-                size=cv_image.shape[:-1],
+            pred = torch.nn.functional.interpolate(
+                pred_depth,
+                size=cv_img.shape[:-1],
                 mode="bicubic",
                 align_corners=False,
             )
 
-            output = prediction.squeeze().cpu().numpy()
+            output = pred.squeeze().cpu().numpy()
 
             output = (output * 1000).astype(np.uint16)
 
             # Convert depth image to ROS message
             depth_msg = self.bridge.cv2_to_imgmsg(output, "16UC1")
-            depth_msg.header = ros_image.header
+            depth_msg.header = ros_img.header
 
             # Publish the depth image
-            self.depth_publisher.publish(depth_msg)
+            self.depth_pub.publish(depth_msg)
+
+            # Publish the camera info
+            self.depth_nn_cam_info_pub.publish(cam_info)
 
         except Exception as e:
             rospy.logerr(e)
@@ -51,8 +71,12 @@ class DepthEstimation:
 
 if __name__ == "__main__":
     try:
-        image_topic = "/naoqi_driver/camera/front/image_raw"
-        depth_node = DepthEstimation(image_topic=image_topic)
+        img_topic = "/naoqi_driver/camera/front/image_raw"
+        cam_info_topic = "/naoqi_driver/camera/front/camera_info"
+        depth_node = DepthEstimation(
+            img_topic=img_topic,
+            cam_info_topic=cam_info_topic,
+        )
         rospy.spin()
     except rospy.ROSInterruptException:
         rospy.loginfo("Depth estimation node terminated.")
