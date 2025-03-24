986b9053b155297a40b9d4dd98899ad6796b197a
“arkadijss” <80927090+arkadijss@users.noreply.github.com>
2025-03-24 17:12:18 +0000

    Add ROS setup to .bashrc only during initial setup

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,11 +10,11 @@ cd catkin_ws
 rosdep update && rosdep install --from-paths src/pepper_mapping -y
 source /opt/ros/$ROS_DISTRO/setup.sh
 catkin_make
-fi
 
 # Add the setup files to bashrc so they are sourced in every new shell
 echo "source /opt/ros/$ROS_DISTRO/setup.sh" >> "/home/$(whoami)/.bashrc"
 echo "source /workspace/catkin_ws/devel/setup.bash" >> "/home/$(whoami)/.bashrc"
+fi
 
 # Start the default command or shell to keep the container running
 exec "$@"
